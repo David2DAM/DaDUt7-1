b5baa3acab11e8fb13064defd8c8c804de621667
“David2DAM” <davidrcgc@gmail.com>
2026-04-23 13:14:47 +0100

    v2

--- src/components/ARdavid.jsx
+++ src/components/ARdavid.jsx
@@ -1,11 +1,30 @@
 import Dashboard from './Dashboard'
 import EjAR from './ar/EjAR'
+import { Typography } from "@mui/material"
+import { Canvas, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 function ARdavid() {
-    return (
+
+    function Scene() {
+      const gltf = useLoader(GLTFLoader, '/scene.gltf')
+
+      return (
+        <primitive scale ={3} object={gltf.scene} position={[0, 0, -5]} />
+    )
+}
+    return (    
         <> 
         <Dashboard/>
-        <EjAR />
+        <Typography variant="h2">Pagina creada por david</Typography>
+
+    <Canvas>
+        <ambientLight intensity={1} />
+        <directionalLight position={[5, 5, 5]} intensity={2} />
+        <Scene />
+      </Canvas>        
+
+
         </>
     );
   }
