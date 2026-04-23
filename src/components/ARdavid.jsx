e4b2358ff98d9061b73c218aa94b3178b05f8061
“David2DAM” <davidrcgc@gmail.com>
2026-04-23 09:59:15 +0100

    v1

---
+++ src/components/ARdavid.jsx
@@ -0,0 +1,13 @@
+import Dashboard from './Dashboard'
+import EjAR from './ar/EjAR'
+
+function ARdavid() {
+    return (
+        <> 
+        <Dashboard/>
+        <EjAR />
+        </>
+    );
+  }
+  
+  export default ARdavid;
